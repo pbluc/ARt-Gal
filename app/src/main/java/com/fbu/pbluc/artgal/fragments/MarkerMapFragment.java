d28f494244b9cd1573d5c786e46292a4ba8da283
Patricia Britney Luc <pbluc@fb.com>
2021-08-03 21:58:43 -0400

    Added BottomNavigationView as well as fragments to the respective activities and layouts for those fragments

---
+++ app/src/main/java/com/fbu/pbluc/artgal/fragments/MarkerMapFragment.java
@@ -0,0 +1,32 @@
+package com.fbu.pbluc.artgal.fragments;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.fbu.pbluc.artgal.R;
+
+
+public class MarkerMapFragment extends Fragment {
+
+  public MarkerMapFragment() {
+    // Required empty public constructor
+  }
+
+  @Nullable
+  @Override
+  public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    return inflater.inflate(R.layout.fragment_marker_map, container, false);
+  }
+
+  @Override
+  public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+    super.onViewCreated(view, savedInstanceState);
+  }
+}
